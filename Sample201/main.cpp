f54c6972daaaada5eac4e9d80882c026aa0c2e22
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2025-07-10 13:29:07 +0900

    20250709

--- Sample201/main.cpp
+++ Sample201/main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
+	FILE* fp;
+	if (fp = fopen("data.txt", "r")) {
+		fclose(fp);
+	}
+	else {
+		cout << "File Open Error!" << endl;
+		return -1;
+	}
 	cout << "Hello World." << endl;
 	return 0;
 }
