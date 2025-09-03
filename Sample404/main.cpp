121e8282cfe6793113946b9d75d2d56f5b0f8446
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2025-09-03 11:18:36 +0900

    20250903

--- Sample404/main.cpp
+++ Sample404/main.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
 int main() {
-	int* p = nullptr;
-	p = new int[10];
+	//int型のポインタ変数を宣言して、new演算子で
+	//要素10個ぶんのint型配列の先頭アドレスを取得
+	int* p = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	//int* p = new int[10]();//すべての要素を0
 	for (int i = 0; i < 10; i++) {
-		p[i] = i;	//p[i]は*(p + i)でも可
+		//*(p + i) = i;
 		cout << p[i] << " ";
 	}
 	cout << endl;
+	//配列が確保していたメモリ領域を解放
 	delete[] p;
 	return 0;
 }
